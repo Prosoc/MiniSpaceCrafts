a1e15a645ad15bd08bfb09e0a09fa758909a9e80
Prosoc <szeplakig91@gmail.com>
2017-08-22 11:15:51 +0200

    Some progress with initial classes

---
+++ MiniSpaceCrafts/Assets/Scripts/Ship.cs
@@ -0,0 +1,102 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Ship{
+
+    public Hull hull;
+
+
+
+
+
+}
+
+
+
+public class Hull
+{
+    public string name;
+    public string type;
+    public int level;
+
+    public float maxHealth;
+    public float health;
+
+    public float maxArmour;
+    public float armour;
+
+    public int roomNum = 1;
+    public Room[] rooms;
+
+    public Hull(string name, string type, int level, float maxHealth, float maxArmour, int roomNum)
+    {
+        this.name = name;
+        this.type = type;
+        this.level = level;
+        this.maxHealth = maxHealth;
+        this.health = maxHealth;
+        this.maxArmour = maxArmour;
+        this.armour = maxArmour;
+        this.roomNum = roomNum;
+
+        this.rooms = new Room[this.roomNum];
+    }
+}
+
+public class Room
+{
+    public float maxHealth;
+    public float health;
+
+    public int roomSize;
+
+    public Room(float maxHealth, float health, int roomSize)
+    {
+        this.maxHealth = maxHealth;
+        this.health = health;
+        this.roomSize = roomSize;
+    }
+}
+
+public class Cockpit : Room
+{
+    public int maxPilotNumber;
+    public Crew[] pilots;
+
+    public Cockpit(float maxHealth, float health, int roomSize, int maxPilotNumber) : base(maxHealth, health, roomSize)
+    {
+
+        this.maxPilotNumber = maxPilotNumber;
+        this.pilots = new Crew[this.maxPilotNumber];
+    }
+}
+
+public class CrewQuarter : Room
+{
+    public int maxCrewNumber;
+
+    public CrewQuarter(float maxHealth, float health, int roomSize, int maxCrewNumber) : base(maxHealth, health, roomSize)
+    {
+        this.maxCrewNumber = maxCrewNumber;
+    }
+}
+
+public class Door {
+
+
+
+}
+
+public class Crew
+{
+    public string name;
+    public float maxHealth;
+    public float health;
+
+    public float pilotingSkill;
+    public float enginesSkill;
+    public float shieldsSkill;
+    public float weaponsSkill;
+}
+
---
+++ MiniSpaceCrafts/Assets/Scripts/ShipGen.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public static class ShipGen {
+
+    static Dictionary<int, string> hulls = new Dictionary<int, string>();
+    static Dictionary<int, string> engines = new Dictionary<int, string>();
+    static Dictionary<int, string> cockpits = new Dictionary<int, string>();
+    static Dictionary<int, string> weaponControlls = new Dictionary<int, string>();
+    static Dictionary<int, string> weapons = new Dictionary<int, string>();
+
+    public static void Data()
+    {
+        //Ship hulls type-#roomSlots-#engineSlots-#weaponSlots
+        hulls.Add(0, "basic-2-1-1");
+        hulls.Add(10, "small-3-2-2");
+        hulls.Add(20, "normal-5-3-4");
+        hulls.Add(30, "big-7-4-6");
+        //Engines
+        engines.Add(0, "basic");
+        engines.Add(10, "small");
+        engines.Add(20, "normal");
+        engines.Add(30, "big");
+        //Cockpits
+        cockpits.Add(0, "basic");
+        cockpits.Add(10, "small");
+        cockpits.Add(20, "normal");
+        cockpits.Add(30, "big");
+        //Weapon Controllers
+        weaponControlls.Add(0, "basic");
+        weaponControlls.Add(10, "small");
+        weaponControlls.Add(20, "normal");
+        weaponControlls.Add(30, "big");
+        //Weapons
+        weapons.Add(0, "basic");
+        weapons.Add(10, "small");
+        weapons.Add(20, "normal");
+        weapons.Add(30, "big");
+    }
+
+
+    public static string GetShip(int level)
+    {
+        string ship = "";
+        
+
+
+        return ship;
+    }
+
+    
+}
